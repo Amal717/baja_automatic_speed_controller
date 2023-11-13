86a4261b584387703d56b5aba74f07c56c917894
Amal717 <64456630+Amal717@users.noreply.github.com>
2023-11-13 12:18:30 +0530

    Update gps.ino

--- gps.ino
+++ gps.ino
@@ -21,8 +21,8 @@ void getSpeed(){
 
 void check_shiftL(){
   if(speed <= upShiftSpeedL){   // check if speed is less than the upshiftSpeedL 15
+    previousMillisL = millis(); // update the timer
     if(millis() - previousMillisL >= downshiftingT){  // check if downshifting time has passed
-        previousMillisL = millis();
         if(speed <= downShiftSpeedM){ // check if the speed has further reduced 10
           shiftL = 1;
           shiftM = 0;
@@ -40,8 +40,8 @@ void check_shiftL(){
 
 void check_shiftM(){
   if(speed >= upShiftSpeedL){  // check if speed is greater than the upshiftSpeedL 15
+    previousMillisM = millis(); // update the timer
     if(millis() - previousMillisM >= upshiftingT){ // check if upshifting time has passed
-      previousMillisM = millis();
       if(speed >= upShiftSpeedL){ // check if the speed is stedy or increasing >= 15
         shiftM = 1;
         shiftL = 0;
@@ -54,8 +54,8 @@ void check_shiftM(){
   }
 
   if(speed <= upShiftSpeedM){ // check if speed is less than downshiftspeedH 25
-    if(millis() - previousMillisH >= downshiftingT){ // check is downshifting time has passed
-      previousMillisH = millis();
+    previousMillisM = millis(); // update the timer 
+    if(millis() - previousMillisM >= downshiftingT){ // check is downshifting time has passed
       if(speed <= downShiftSpeedH){  // check if speed is further reduced, less than 20
         shiftM = 1;
         shiftL = 0;
@@ -71,8 +71,8 @@ void check_shiftM(){
 
 void check_shiftH(){
   if(speed >= upShiftSpeedM){ // check if speed is greater than upshiftspeedM 25
+    previousMillisH = millis(); // update the timer
     if(millis() - previousMillisH >= upshiftingT){ // check if upshifting time has passed
-      previousMillisH = millis();
       if(speed >= upShiftSpeedM){ // check if speed is steady or increasing, greater than 25
         shiftH = 1;
         shiftM = 0;
@@ -84,8 +84,3 @@ void check_shiftH(){
     shiftH = 0;
   }
 }
-
-
-
-
-
